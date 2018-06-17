dd32c420a1ae4a87bf5c93baac478b2c4a4c920b
Ryan Manuel <rfmanuel@gmail.com>
2018-06-17 09:41:32 -0500

    Add eslint and danger to package.json (#2)

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "react",
-    "jsx-a11y",
-    "import",
-    "compat"
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+    'compat',
   ],
-  "env": {
-    "browser": true,
-    "jest": true
+  env: {
+    browser: true,
+    jest: true,
   },
-  "rules": {
-    "max-len": "off", // Remove warnings on max line length exceeding 100 characters
-    "react/require-default-props": "off", // Disabled the requirement to default all non-required props
-    "compat/compat": "error"
+  rules: {
+    'max-len': 'off', // Remove warnings on max line length exceeding 100 characters
+    'react/require-default-props': 'off', // Disabled the requirement to default all non-required props
+    'compat/compat': 'error',
   },
-  "globals": {
-    "shallow": true,
-    "render": true,
-    "mount": true
+  globals: {
+    shallow: true,
+    render: true,
+    mount: true,
   },
 };
