934896d8f85080bfeb8e002979ca569a43ee13cf
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-06-21 13:38:13 -0500

    Disable forbidden proptypes rule

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     'max-len': 'off', // Remove warnings on max line length exceeding 100 characters
+    'react/forbid-prop-types': 'off', // Disabled to allow for the use of any React PropType
     'react/require-default-props': 'off', // Disabled the requirement to default all non-required props
     'compat/compat': 'error',
   },
