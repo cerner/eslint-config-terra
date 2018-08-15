a3e3a39d53b79959d699799c128ac80e6fe7576f
Brett Jankord <brett.jankord@cerner.com>
2018-08-15 10:17:03 -0500

    Update rules

--- eslint.config.js
+++ eslint.config.js
@@ -11,14 +11,21 @@ module.exports = {
     jest: true,
   },
   rules: {
-    'max-len': 'off', // Remove warnings on max line length exceeding 100 characters
-    'react/require-default-props': 'off', // Disabled the requirement to default all non-required props
+    // Remove warnings on max line length exceeding 100 characters
+    'max-len': 'off',
+    // Disabled the requirement to default all non-required props
+    'react/require-default-props': 'off',
     'compat/compat': 'error',
-    'jsx-a11y/label-has-for': [2, {
-      required: {
-        some: ['nesting', 'id'],
-      },
+    // Disable this rule as it has been marked as deprecated in jsx-a11y plugin
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/releases/tag/v6.1.0
+    'jsx-a11y/label-has-for': 'off',
+    // Replaces jsx-a11y/label-has-for rule. By default, it wants inputs to be both wrapped in a label
+    // and include a id/for attribute mapping with label.
+    // This config updates the rule to require one or the other.
+    "jsx-a11y/label-has-associated-control": [2, {
+      "assert": "either"
     }],
+    "prefer-destructuring": 'off',
   },
   globals: {
     shallow: true,
