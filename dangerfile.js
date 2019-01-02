9788f13b325d7a3eecac18d9d6f8df104af004ac
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-01-02 11:19:37 -0600

    add

--- dangerfile.js
+++ dangerfile.js
@@ -1,23 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars, object-curly-newline
-import { danger, warn, fail, message } from 'danger';
-
-const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
-  const srcFilePattern = /CHANGELOG.md/i;
-  return srcFilePattern.test(filePath);
-});
-
-const modifiedConfigFiles = danger.git.modified_files.filter((filePath) => {
-  const scriptFilePattern = /eslint.config.js/i;
-  return scriptFilePattern.test(filePath);
-});
-
-const hasCHANGELOGChanges = modifiedChangelog.length > 0;
-const hasModifiedConfigFiles = modifiedConfigFiles.length > 0;
-
-// Fail if there are src code changes without a CHANGELOG update
-if (hasModifiedConfigFiles && !hasCHANGELOGChanges) {
-  fail('Please include a CHANGELOG entry with this PR.');
-}
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { danger, warn } from 'danger';
 
 // Warn when there is a big PR
 const bigPRThreshold = 1000;
