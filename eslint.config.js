bf08e474198e4c6852665c83f63d44fd830c7bfb
Brett Jankord <brett.jankord@cerner.com>
2019-08-12 06:24:54 -0500

    Disable rules

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,8 @@ module.exports = {
     // when using eslint-plugin-react 7.12.2 or above.
     'react/jsx-wrap-multilines': 'warn',
     'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/state-in-constructor': 'off',
   },
   settings: {
     polyfills: ['object-values'],
